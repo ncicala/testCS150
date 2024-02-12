d32f7ca6deec8d3814b16cd1b2c310566b963f6c
ncicala <156989042+ncicala@users.noreply.github.com>
2024-02-12 17:22:35 +0000

    added while loop

--- main.cpp
+++ main.cpp
@@ -7,6 +7,12 @@ int main()
 {
     cout << "Hello World!" << endl;
     void name();
+    int i = 0;
+    while (i < 5)
+    {
+        cout << "Meow\n";
+        i++;
+    }
     return 0;
 }
 void name()
